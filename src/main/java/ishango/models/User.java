0663924942ad58609bf58e80900830cc06d73836
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-07-29 02:04:22 -0700

    Add User and Vote models

---
+++ src/main/java/ishango/models/User.java
@@ -0,0 +1,39 @@
+package ishango.models;
+
+import java.util.Objects;
+
+public class User {
+  private final Integer id;
+  private final String name;
+
+  public User(Integer id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Candidate)) return false;
+    Candidate candidate = (Candidate) o;
+    return getId().equals(candidate.getId()) && getName().equals(candidate.getName());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getName());
+  }
+
+  @Override
+  public String toString() {
+    return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
+  }
+}
